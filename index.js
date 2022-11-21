88b48ed329aa30089dc6057bc6667397166c323d
Francisco Harris <francisco.harris@globant.com>
2022-11-21 16:16:46 -0300

    Change language

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-import axios from "axios";
-import {get, URLS} from "./config/url";
+import axios from 'axios';
+import { get, URLS } from './config/url';
 
-const {TRANSLATE} = URLS;
+const { TRANSLATE } = URLS;
 
 const encodedParams = new URLSearchParams();
-encodedParams.append("q", "Hello, world!");
-encodedParams.append("target", "es");
-encodedParams.append("source", "en");
+encodedParams.append('q', 'Hola, mundo!');
+encodedParams.append('target', 'es');
+encodedParams.append('source', 'en');
 
 const options = {
-    method: 'POST',
-    url: get(TRANSLATE),
-    headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
-        'X-RapidAPI-Key': 'dbed9573c9mshcbd31318ee4c098p1bbf3cjsn280dc67a63bf',
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    },
-    data: encodedParams
+  method: 'POST',
+  url: get(TRANSLATE),
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'Accept-Encoding': 'application/gzip',
+    'X-RapidAPI-Key':
+      'dbed9573c9mshcbd31318ee4c098p1bbf3cjsn280dc67a63bf',
+    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+  },
+  data: encodedParams,
 };
 
-axios.request(options).then(function (response) {
+axios
+  .request(options)
+  .then(function (response) {
     console.log(response.data.data.translations);
-}).catch(function (error) {
+  })
+  .catch(function (error) {
     console.error(error);
-});
+  });
